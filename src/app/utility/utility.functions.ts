aec085a3edd8e7a9e3ce6a997d2cabee11460ffa
Elvis <elvisdurmishi@gmail.com>
2022-07-15 15:50:07 +0200

    helpers

---
+++ src/app/utility/utility.functions.ts
@@ -0,0 +1,11 @@
+import {JWT} from "../model/JWT";
+
+export function parseJwt (token: string): JWT {
+  let base64Url = token.split('.')[1];
+  let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  let jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
